e3f209193bcc77232c09676009f9593eb7f45f1c
Everton Assis Correia <evtcorreia@gmail.com>
2025-03-05 23:14:17 -0300

    Start Project

--- App.tsx
+++ App.tsx
@@ -1,131 +1,133 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  ScrollView,
-  StatusBar,
+  SafeAreaView,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+//const [state, setState] = React.useState({displayValue: '0', clearDisplay: false, operation: null, values: [0, 0], current: 0});
+// const [operation, setOperation] = React.useState('');
+const [state, setState] = React.useState<{
+  displayValue: string;
+  clearDisplay: boolean;
+  operation: string | null;
+  values: number[];
+  current: number;
+}>({
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+});
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+function addDigit(digit: string) {
+  if (digit === '.' && state.displayValue.includes('.')) {
+    return;
+  }
+
+  const clearDisplay = state.displayValue === '0' || state.clearDisplay;
+  const currentValue = clearDisplay ? '' : state.displayValue;
+  const displayValue = currentValue + digit;
+
+  const newValue = digit !== '.' ? parseFloat(displayValue) : state.values[state.current];
+  const values = [...state.values];
+  values[state.current] = newValue;
+
+  setState(prevState => ({
+    ...prevState,
+    displayValue,
+    clearDisplay: false,
+    values,
+  }));
+}
+
+function clearMemory() {
+  setState({
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0,
+  });
+}
+
+function setOperation(operation: string) {
+  if (state.current === 0) {
+    setState(prevState => ({
+      ...prevState,
+      operation,
+      current: 1,
+      clearDisplay: true,
+    }));
+  } else {
+    const equals = operation === '=';
+    const values = [...state.values];
+
+    if (state.operation) {
+      try {
+        values[0] = eval(`${values[0]} ${state.operation} ${values[1]}`);
+      } catch (error) {
+        values[0] = state.values[0];
+      }
+    }
+
+    values[1] = 0;
+
+    setState(prevState => ({
+      ...prevState,
+      displayValue: values[0].toString(),
+      operation: equals ? null : operation,
+      current: equals ? 0 : 1,
+      clearDisplay: !equals,
+      values,
+    }));
+  }
+}
+
+
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
+    <SafeAreaView style={styles.container}>
+    <Display value = {state.displayValue}/>
+      <SafeAreaView style={styles.buttons}>
+      <Button label = "AC" triple onClick={() => clearMemory()}/>
+      <Button label = "/" operation onClick={() => setOperation('/')} />
+      <Button label = "7" onClick = {() => addDigit('7')}/>
+      <Button label = "8" onClick= {() => addDigit('8')}/>
+      <Button label = "9" onClick={() => addDigit('9')} />
+      <Button label = "*" operation onClick={() => setOperation('*')}/>
+      <Button label = "4" onClick={() => addDigit('4')} />
+      <Button label = "5" onClick={() => addDigit('5')}/>
+      <Button label = "6" onClick={() => addDigit('5')}/>
+      <Button label = "-" operation onClick={() => setOperation('-')}/>
+      <Button label = "1" onClick={() => addDigit('1')}/>
+      <Button label = "2" onClick={() => addDigit('2')}/>
+      <Button label = "3" onClick={() => addDigit('3')}/>
+      <Button label = "+" operation onClick={() => setOperation('+')} />
+      <Button label = "0" double onClick={() => addDigit('0')} />
+      <Button label = "." onClick={() => addDigit('.')} />
+      <Button label = "=" operation onClick={() => setOperation('=')}/>
+
+    </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: '#F5FCFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }});
 
 export default App;
